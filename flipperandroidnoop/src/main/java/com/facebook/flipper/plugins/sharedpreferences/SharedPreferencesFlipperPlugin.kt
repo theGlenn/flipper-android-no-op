d016577e9dfa88777c06faab806d3afa61f52c2b
Vladislav Nikolaev <nikolaev.vladis@gmail.com>
2020-04-15 00:17:57 +0300

    add overloaded constructors to SharedPreferencesFlipperPlugin

--- flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/sharedpreferences/SharedPreferencesFlipperPlugin.kt
+++ flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/sharedpreferences/SharedPreferencesFlipperPlugin.kt
@@ -2,4 +2,14 @@ package com.facebook.flipper.plugins.sharedpreferences
 
 import android.content.Context
 
-class SharedPreferencesFlipperPlugin(context: Context)
+class SharedPreferencesFlipperPlugin {
+
+    constructor(context: Context)
+
+    @JvmOverloads
+    constructor(context: Context, name: String, mode: Int = Context.MODE_PRIVATE)
+
+    constructor(context: Context, descriptors: List<SharedPreferencesDescriptor>)
+
+    class SharedPreferencesDescriptor(name: String, mode: Int)
+}
