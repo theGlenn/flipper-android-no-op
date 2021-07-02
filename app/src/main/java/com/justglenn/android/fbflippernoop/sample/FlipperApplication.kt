d49610920dd2bc64081301b3847ec56254223df3
Glenn Sonna <kenpachi.os@gmail.com>
2021-07-02 13:13:59 +0200

    Update FlipperApplication.kt

--- app/src/main/java/com/justglenn/android/fbflippernoop/sample/FlipperApplication.kt
+++ app/src/main/java/com/justglenn/android/fbflippernoop/sample/FlipperApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabaseDescriptor
+import com.facebook.flipper.plugins.databases.DatabaseDriver
 import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
 import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseDriver
 import com.facebook.flipper.plugins.databases.impl.SqliteDatabaseProvider
@@ -16,6 +18,7 @@ import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import com.facebook.soloader.SoLoader
 import leakcanary.LeakCanary
+
 
 class FlipperApplication : Application() {
 
@@ -34,19 +37,59 @@ class FlipperApplication : Application() {
                 onHeapAnalyzedListener = FlipperLeakListener()
             )
 
-            AndroidFlipperClient.getInstance(this).apply {
-                addPlugin(NetworkFlipperPlugin())
-                addPlugin(InspectorFlipperPlugin(this@FlipperApplication, DescriptorMapping.withDefaults()))
-                addPlugin(DatabasesFlipperPlugin(this@FlipperApplication))
-                addPlugin(DatabasesFlipperPlugin(SqliteDatabaseDriver(this@FlipperApplication, SqliteDatabaseProvider {
-                    emptyList() //database file list
-                })))
-                addPlugin(SharedPreferencesFlipperPlugin(this@FlipperApplication))
-                addPlugin(LeakCanary2FlipperPlugin())
-                addPlugin(NavigationFlipperPlugin.getInstance())
-                addPlugin(CrashReporterPlugin.getInstance())
-                start()
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(NetworkFlipperPlugin())
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+
+            client.addPlugin(SharedPreferencesFlipperPlugin(this))
+            client.addPlugin(LeakCanary2FlipperPlugin())
+            client.addPlugin(NavigationFlipperPlugin.getInstance())
+            client.addPlugin(CrashReporterPlugin.getInstance())
+
+            // DatabasesFlipperPlugin
+            client.let {
+                it.addPlugin(DatabasesFlipperPlugin(this))
+                it.addPlugin(
+                    DatabasesFlipperPlugin(
+                        SqliteDatabaseDriver(
+                            this,
+                            SqliteDatabaseProvider { emptyList() })
+                    )
+                )
+
+                it.addPlugin(DatabasesFlipperPlugin(object :
+                    DatabaseDriver<DatabaseDescriptor>(this) {
+                    override fun getDatabases(): MutableList<DatabaseDescriptor>? = null
+                    override fun getTableNames(databaseDescriptor: DatabaseDescriptor): List<String?>? =
+                        null
+
+                    override fun getTableData(
+                        databaseDescriptor: DatabaseDescriptor,
+                        table: String?,
+                        order: String?,
+                        reverse: Boolean,
+                        start: Int,
+                        count: Int
+                    ): DatabaseGetTableDataResponse? = null
+
+                    override fun getTableStructure(
+                        databaseDescriptor: DatabaseDescriptor,
+                        table: String?
+                    ): DatabaseGetTableStructureResponse? = null
+
+                    override fun executeSQL(
+                        databaseDescriptor: DatabaseDescriptor,
+                        query: String?
+                    ): DatabaseExecuteSqlResponse? = null
+
+                    override fun getTableInfo(
+                        databaseDescriptor: DatabaseDescriptor,
+                        table: String?
+                    ): DatabaseGetTableInfoResponse? = null
+                }))
             }
+
+            client.start()
         }
     }
 }
