8aa31d02ab4800b78c80cec7d60714bc753afd40
Arga Asasta <asasta7@gmail.com>
2021-10-19 17:36:57 +0700

    Update FlipperOkhttpInterceptor.kt
    Can we add the maxBodyBytes and mock for same implementation on the flipper?

--- flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/network/FlipperOkhttpInterceptor.kt
+++ flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/network/FlipperOkhttpInterceptor.kt
@@ -3,7 +3,7 @@ package com.facebook.flipper.plugins.network
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class FlipperOkhttpInterceptor(private val plugin: NetworkFlipperPlugin) : Interceptor {
+class FlipperOkhttpInterceptor(private val plugin: NetworkFlipperPlugin, private val long maxBodyBytes, private val boolean isMockResponseSupported) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return chain.proceed(chain.request())
     }
