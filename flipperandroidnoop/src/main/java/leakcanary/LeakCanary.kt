ed0702e3db81383b8a8bf269c3805ace1e900838
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2023-04-29 12:36:56 -0400

    Update LeakCanary.kt

--- flipperandroidnoop/src/main/java/leakcanary/LeakCanary.kt
+++ flipperandroidnoop/src/main/java/leakcanary/LeakCanary.kt
@@ -4,7 +4,9 @@ object LeakCanary {
 
   data class Config(
 
+    @Deprecated(message = "Add to LeakCanary.config.eventListeners instead")
     val onHeapAnalyzedListener: Any? = null
+    val eventListeners: List<EventListener> = listOf(),
 
   )
   @JvmStatic @Volatile
